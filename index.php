445b523fe87f803990de6bbb4e7554f32b0bd006
CaguCTjkee <caguct.mac@yandex.ru>
2017-05-26 20:19:30 +0300

    test float

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Hello world';
+//echo 'Hello world';
+
+$x = 8.0 - 6.4;
+$y = 1.6;
+
+var_dump(round($x, 2) == round($y, 2)); // false
+
+echo '<br>';
+
+var_dump($x);
+
+echo '<br>';
+
+var_dump($y);
